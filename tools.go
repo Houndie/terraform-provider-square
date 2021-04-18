28def917092570de1921c2dfd96d47f8c584a339
James Benze <benzejaa@gmail.com>
2021-04-18 11:59:57 -0400

    Pin goreleaser version with toolbox

---
+++ tools.go
@@ -0,0 +1,8 @@
+// +build tools
+
+// This file is generated and managed by toolbox.  Manually edit at your own peril.
+package toolbox
+
+import (
+	_ "github.com/goreleaser/goreleaser@v0.162.0"
+)
